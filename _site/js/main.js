244b964ddcb8cc388c17d2f61c10cda3f37d02a3
Caleb Mercer <kmercinary@gmail.com>
2014-02-08 14:34:17 +1100

    Create Jekyll structure with bootstrap and less

---
+++ _site/js/main.js
@@ -0,0 +1,19 @@
+(function(){
+
+	// var appModel = function(){
+	// 	var self = this;
+
+	// 	self.firstName = ko.observable("");
+	// 	self.lastName = ko.observable("");
+	// 	self.field1HasFocus = ko.observable(false);
+	// 	self.field2HasFocus = ko.observable(false);
+
+	// 	self.fullName = ko.computed(function(){
+	// 		return self.firstName() + " " + self.lastName();
+	// 	}, self);
+
+
+	// };
+
+	// ko.applyBindings(new appModel);
+})();
